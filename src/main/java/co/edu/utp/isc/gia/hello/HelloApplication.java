1de7184f14a53aea4a804199c4b26a94c414ef74
ahsan <ahsan@ahsan-ManjaroXFCE>
2020-03-30 16:58:46 -0500

    Servicio Rest

---
+++ src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
@@ -0,0 +1,33 @@
+package co.edu.utp.isc.gia.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("hello")
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+        
+        @GetMapping("/{firstName}/{lastName}")
+        public String sayHello(
+                @PathVariable("firstName") String firstName, 
+                @PathVariable("lastName") String lastName){
+            return String.format("Hello %s %s", firstName, lastName);
+        }
+        
+        @GetMapping("/ensayo")
+        public String sayHello2(@RequestParam("nombre") String nombre, @RequestParam("apellido") String apellido){
+            return String.format("Hello %s %s", nombre, apellido);
+        }
+
+}
+
